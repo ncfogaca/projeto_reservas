b4edad39c475d47c5e2a879aed456eab777ae842
Dieniffer Tavares <dienitavares@gmail.com>
2015-06-15 23:24:24 -0300

    Correção alteração anterior

--- db/migrate/20150616014142_create_hotels.rb
+++ db/migrate/20150616014142_create_hotels.rb
@@ -2,7 +2,7 @@ class CreateHotels < ActiveRecord::Migration
   def change
     create_table :hotels do |t|
       t.string :name
-      t.reference :id_local
+      t.references :id_local
 
       t.timestamps null: false
     end
